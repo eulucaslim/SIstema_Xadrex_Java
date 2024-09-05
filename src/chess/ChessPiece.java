124702f1d0914d79332ca4917c4e6291a4f6a89b
eulucaslim <lucaslimacnt14@gmail.com>
2024-09-04 23:49:59 -0400

    Placing pieeces on the board

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,26 +6,16 @@ import boardgame.Piece;
 public class ChessPiece extends Piece {
 	
 	private Color color;
-	private int moveCount;
 	
-	public ChessPiece(Board board, Color color, int moveCount) {
+	
+	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
-		this.moveCount = moveCount;
+		
 	}
 
 	public Color getColor() {
 		return color;
 	}
 
-	public int getMoveCount() {
-		return moveCount;
-	}
-
-	public void setMoveCount(int moveCount) {
-		this.moveCount = moveCount;
-	}
-	
-	
-	
 }
